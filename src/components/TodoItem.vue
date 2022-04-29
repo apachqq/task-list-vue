86fffc7ede922fffb06927dc7a8cb54c0afa346e
apachqq <work.tsyganov@mail.ru>
2022-04-29 10:42:36 +0300

    dynamic-class

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,6 +1,11 @@
 <template>
     <li>
-        {{ todo.title }}
+        <span :class="{done: todo.completed}">
+            <input type="checkbox" @change="todo.completed = !todo.completed">
+            <strong>{{ todo.id }}</strong>
+            {{ todo.title }}
+        </span>
+        <button class="rm">&times;</button>
     </li>
 </template>
 
@@ -16,4 +21,26 @@
 </script>
 
 <style scoped>
+    li {
+        border: 1px solid #ccc;
+        display: flex;
+        justify-content: space-between;
+        padding: .5rem 2rem;
+        margin-bottom: 1rem;
+    }
+
+    .rm {
+        background: red;
+        color: #fff;
+        border-radius: 50%;
+        font-weight: bold;
+    }
+
+    .done {
+        text-decoration: line-through;
+    }
+
+    input {
+        margin-right: 1rem;
+    }
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -23,4 +23,9 @@
 </script>
 
 <style scoped>
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
 </style>
