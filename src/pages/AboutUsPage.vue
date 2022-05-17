f1faf6b19295f361bf9dfc51c38ee56c0f863349
apachqq <work.tsyganov@mail.ru>
2022-05-17 14:01:42 +0300

    aboutUsFix

--- src/pages/AboutUsPage.vue
+++ src/pages/AboutUsPage.vue
@@ -2,7 +2,6 @@
     <div>
         <h2>About us</h2>
         <p>The application was created in order to learn new skills and consolidate old skills</p>
-        <router-link to="/">Go to main page</router-link>
     </div>
 </template>
 
