2d984c2d8ceb711ea72bd2f1d584602e10ac3cdd
apachqq <work.tsyganov@mail.ru>
2022-05-17 13:04:54 +0300

    LoginPage

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card">
+        <h2>Авторизуйтесь</h2>
+        <form @submit.prevent="submit">
+            <div class="input">
+                <input type="email" placeholder="Введите email" v-model="email">
+            </div>
+            <div class="input">
+                <input type="password" placeholder="Введите пароль" v-model="password">
+            </div>
+            <my-button style="margin-top: 20px;">Авторизуйтесь</my-button>
+        </form>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+    .card {
+        text-align: center;
+    }
+
+    input {
+        padding: 10px;
+        margin-top: 15px;
+        border: 0;
+        box-shadow: 0 0 15px 4px rgba(0, 0, 0, 0.06);
+        border-radius: 10px;
+        width: 20%;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,27 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage'
 import TodosPage from '@/pages/TodosPage'
 import AboutUsPage from '@/pages/AboutUsPage'
+import LoginPage from '@/pages/LoginPage'
 import PageNotFound from '@/pages/PageNotFound'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {
-            path: '/',
-            component: HomePage
-        },
-        {
-            path: '/todos',
-            component: TodosPage
-        },
-        {
-            path: '/about',
-            component: AboutUsPage
-        },
-        {
-            path: '/:notFound(.*)',
-            component: PageNotFound
-        }
+        {path: '/', component: HomePage},
+        {path: '/todos', component: TodosPage},
+        {path: '/about', component: AboutUsPage},
+        {path: '/login', name: 'LoginPage', component: LoginPage},
+        {path: '/:notFound(.*)', component: PageNotFound}
     ]
 })
 
